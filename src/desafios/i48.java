c199ce55839c4572f6c4d8b93eff892677acaa2c
minipitusas <quirozciro00@gmail.com>
2024-06-14 12:03:27 -0300

    14/06

--- src/desafios/i48.java
+++ src/desafios/i48.java
@@ -1,5 +1,44 @@
 package desafios;
 
-public class i48 {
+import java.util.Scanner;
 
+public class i48 {
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		int[] numeros = new int[4];
+
+		for (int i = 0; i < 4; i++) {
+			System.out.println("Ingrese un DNI: ");
+			numeros[i] = entrada.nextInt();
+
+		}
+
+		System.out.println("Arreglo desordenado:");
+		for (int num : numeros) {
+			System.out.print(num + " ");
+		}
+
+		for (int i = 0; i < (numeros.length - 1); i++) {
+
+			for (int j = 0; j < (numeros.length - i - 1); j++) {
+
+				if (numeros[j] > numeros[j + 1]) {
+
+					// Intercambiar los elementos
+					int temporal = numeros[j];
+
+					numeros[j] = numeros[j + 1];
+
+					numeros[j + 1] = temporal;
+				}
+				
+				System.out.println("\nArreglo ordenado:");
+				for (int num : numeros) {
+					System.out.print(num + " ");
+				}
+
+			}
+		}
+	}
 }
